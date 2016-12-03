857ddec906e4a44b47ab9aab88d28ca13da078fb
Edgar Orendain <edgar@orendainx.com>
2016-12-03 02:19:31 -0800

    Created midwest and new york route files.

--- src/main/scala/com/hortonworks/orendainx/trucking/simulator/collectors/EventCollector.scala
+++ src/main/scala/com/hortonworks/orendainx/trucking/simulator/collectors/EventCollector.scala
@@ -17,6 +17,6 @@ trait EventCollector extends Actor
 
 /*
  * A valid concern could be that by using actors over some singleton service that is equally as threadsafe, we are employing an antipattern.
- * However, considering not all EventCollectors may leverage a datastructure that can be accessed concurrently, using actors
+ * However, considering not all EventCollectors may leverage a data structure that can be accessed concurrently, using actors
  * to regulate resource access is a much better way (and cleaner) way to approach the problem.
  */
